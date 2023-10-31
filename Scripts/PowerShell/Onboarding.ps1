ed87e7cd4d28ede47f8679f955bdbd59e4ee717a
Josh Richards <116070770+twosharprocks@users.noreply.github.com>
2023-10-31 21:40:37 +1030

    Update Onboarding.ps1

--- Scripts/PowerShell/Onboarding.ps1
+++ Scripts/PowerShell/Onboarding.ps1
@@ -1,22 +1,20 @@
-#New User AD Attributes below
+#Add in the Active Directory Attributes of the new user below
 $NewUser = 'username'
 $sourceUser = 'userToDrawPermissionsFrom' #Reference the pre-2000 username
 $manager = 'usersManager' #Reference the pre-2000 username
 $jobtitle = 'jobtitle'
 $office = "Office Address"
-$department = "user department"
+$department = "user's department"
 $streetAddress = "Adrress"
 $city = 'city'
 $postalCode = 'postcode'
-
-#Code starts here - do not edit below this point
+#Set attributes below that are standard for all users 
+Set-ADUser -Identity $NewUser -Office $office -department $department -streetAddress $streetAddress -state SA -city $city -PostalCode $postalCode -description $jobtitle -title $jobtitle -manager $manager -company "Company Name" -Replace @{c="CountryCode";wWWHomePage="http:\\www.companywebsite.com"} -HomeDirectory $HomeDirectory -HomeDrive "F:"
 #Create a Home Directory Variable
 $ADIdentity = Get-ADUser -Identity $NewUser
 $BasePath = "\\DomainController\UserFiles\HomeDirectory\"
 $HomeDirectory = $BasePath + $ADIdentity.SamAccountName
-
-#Copy Group Permissions from sourcename to New User
-Set-ADUser -Identity $NewUser -Office $office -department $department -streetAddress $streetAddress -state SA -city $city -PostalCode $postalCode -description $jobtitle -title $jobtitle -manager $manager -company "Company Name" -Replace @{c="CountryCode";wWWHomePage="http:\\www.companywebsite.com"} -HomeDirectory $HomeDirectory -HomeDrive "H:"
+#Copy Group Permissions from sourceUser to NewUser
 $sourceGroups = Get-ADUser -Identity $sourceUser -Properties MemberOf | Select-Object -ExpandProperty MemberOf
 foreach ($group in $sourceGroups) {
    # Add the group to the target user
