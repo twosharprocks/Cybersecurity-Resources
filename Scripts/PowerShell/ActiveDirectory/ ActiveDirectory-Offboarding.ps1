a49f5957fa775c3b978dd359e1c05684df7834ae
Josh Richards <116070770+twosharprocks@users.noreply.github.com>
2024-05-29 19:23:54 +0930

    Update

---
+++ Scripts/PowerShell/ActiveDirectory/ ActiveDirectory-Offboarding.ps1
@@ -0,0 +1,27 @@
+#Removing a user from their group memberships and updating their description
+$user = 'username'
+$updatedDescription = 'Offboarded on dd=mm-yyyy'
+$destinationOU = "OU=Off Boarding,OU=Users,OU=Users & Groups,DC=domain,DC=suffix"
+# Get the user object & update their description
+Get-ADUser -Identity $user | Set-ADUser -description $updatedDescription
+if ($user -eq $null) {
+    Write-Host "User not found."
+    exit
+}
+# Move the user to the new OU
+Get-ADUser -Identity $user | Move-ADObject -TargetPath $destinationOU
+# Get the group memberships of the user
+$userGroups = Get-ADPrincipalGroupMembership -Identity $user
+# Loop through the user's group memberships and remove them, excluding "Domain Users"
+foreach ($group in $userGroups) {
+    if ($group.objectClass -eq 'group' -and $group.Name -ne "Domain Users") {
+        # Use Try-Catch to handle any potential errors (e.g., if the user is not a member of the group)
+        try {
+            Remove-ADGroupMember -Identity $group -Members $user -Confirm:$false
+            Write-Host "Removed $($user.SamAccountName) from $($group.Name)"
+        } catch {
+            Write-Host "Failed to remove $($user.SamAccountName) from $($group.Name)"
+        }
+    }
+}
+Write-Host "$user moved to Off Boarding and Group memberships (excluding 'Domain Users') removed"
