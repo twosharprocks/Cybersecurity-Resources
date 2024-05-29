b11d0f7044e4f9c5e3a5c8b8cd00e450ae2281ec
Josh Richards <116070770+twosharprocks@users.noreply.github.com>
2024-05-29 19:53:18 +0930

    Update and rename ActiveDirectory-EmailAddresses.ps1 to AD-EmailToUser.ps1

--- Scripts/PowerShell/ActiveDirectory/AD-EmailToUser.ps1
+++ Scripts/PowerShell/ActiveDirectory/AD-EmailToUser.ps1
@@ -1,34 +1,17 @@
-# The following is a series of scripts that can be used to:
-# (1) Find email addresses in a large amount of text, 
-# (2) Format those email addresses so PowerShell can search for them in Active Directory
-# (3) Search AD for accounts matching those email addresses and display the SAM Account Name
-# (4) Search AD for users with those email addresses inside different OUs & if they're enabled/disabled
----
-## (1) Find emails in text
-#Text you want to search for email addresses
-$text = @"
-Copy and paste in here the text you want to search for email addresses
-"@
-# Regular expression pattern for finding email addresses
+# The following is a series of scripts that can be used to find email addresses in text, then search for Active Directory users with matching emails and determine if they're enabled or disabled.
+$text = Read-Host 'Paste the block of text to search for email addresses here:'
 $emailPattern = '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
-# Use Select-String to find email addresses in the text
 $matches = $text | Select-String -Pattern $emailPattern -AllMatches | ForEach-Object { $_.Matches.Value }
----
-## (2) Format emails so PowerShell can search for them in Active Directory
-#Add " to the start and ," to the end of each line
 $emailAddresses = $matches -replace '(.+)', '"$1",'
-# Display the emailAddresses variable
 Write-Host $emailAddresses
-# Copy the output but exclude the comma on the final line, and redefine $emailAddresses using 2.1 below
----
+# Copy the output from above, but exclude the comma on the final line, then redefine $emailAddresses using 2.1 below;
 ## (2.1) Creating an "emailAddresses" variable if you already have the list formatted correctly
 $emailAddresses = @(
 "email1@company.co",
 "email2@gmail.com",
 "email3@notforprofit.org"
 )
----
-## (3) Searching AD for users with email addresses found in $emailAddresses
+## (3) Search AD for users with email addresses found in $emailAddresses
 foreach ($email in $emailAddresses) {
 	$user = Get-ADUser -Filter
 	if ($user -ne $null) {
@@ -38,10 +21,8 @@ foreach ($email in $emailAddresses) {
 	Write-Output "$email does not match any user"
 	}
 }
----
 ## (4.1) Enabled AD Users with email addresses listed in the emailAddresses variable
-write-host "--- Enabled Users with emails matches ---"
-write-host ""
+write-host "--- Enabled Users found with matching email addresses ---"
 foreach ($email in $emailAddresses) {
     $user = Get-ADUser -Filter {EmailAddress -eq $email} -Properties Enabled
 	if ($user) {
@@ -52,18 +33,19 @@ foreach ($email in $emailAddresses) {
         Write-Host "$email not found in Active Directory."
     }
 }
----
 ## (4.2) Enabled AD Users from a specific Domain group (eg. Administrators) with email address matches 
 #Define $ouPath for which Group to search (eg. Administrators, Office, Inactive, Resources, ect)
-$ouPath = "OU=Administrators,OU=Users,OU=_Users & Groups,DC=acm,DC=int"
+$ouPath = "OU=AdministratorsDC=int"
 #Disabled users with matching emails
-write-host ""
-write-host "--- Enabled Users in $ouPath with matching email addresses ---"
+write-host "--- Users in $ouPath with matching email addresses ---"
 write-host ""
 #Filter and print user with description
 foreach ($email in $emailAddresses) {
     $user = Get-ADUser -Filter {EmailAddress -eq $email} -SearchBase "OU=Administrators,OU=Users,OU=_Users & Groups,DC=acm,DC=int"
 	if ($user.Enabled -eq $true) {
-            Write-Host "User: $($user.SamAccountName) Description: $($user.Description)"
-        } else {}
+            Write-Host "Enabled - $($user.SamAccountName) Description: $($user.Description)"
+        } else {
+	Write-Host "Disabled - $($user.SamAccountName) Description: $($user.Description)"
+	}
+}
 }
